e218766231ee3f89868cdbea3621d4812f5a9add
Mishu Goyal <mishugoyal11@gmail.com>
2017-12-24 17:00:47 +0530

    better layout, hover icon change, async call, data, api call fail handle

--- js/app.js
+++ js/app.js
@@ -1,50 +1,79 @@
 var locations_data = [
-  {title: 'Mill Creek Park', location: {lat: 41.098927, lng: -80.676628}},
-  {title: 'Edisto Beach state Park', location: {lat: 32.510651, lng: -80.286995}},
-  {title: 'Jacobson Park', location: {lat: 37.987938, lng: -84.419792}},
-  {title: 'Spring city Park', location: {lat: 35.688900, lng: -84.853401}},
-  {title: 'Jungle Hut Road Park', location: {lat: 29.584776, lng: -81.177689}},
-  {title: 'Anastasia state Park', location: {lat: 29.875853, lng: -81.280891}},
-  {title: 'Juno Beach Park', location: {lat: 26.893070, lng: -80.058220}},
-  {title: 'Seward Park', location: {lat: 47.550259, lng: -122.264847}},
-  {title: 'Northacres Park', location: {lat: 47.721790, lng: -122.327209}},
-  {title: 'Myrtle Edwards Park', location: {lat: 47.620598, lng: -122.361816}},
-  {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+  {title: 'Statue of Liberty', location: {lat: 40.689249, lng: -74.044500}},
+  {title: 'Central Park', location: {lat: 40.782865, lng: -73.965355}},
+  {title: 'Rockefeller Center and Top of the Rock Observation Deck', location: {lat: 40.759243, lng: -73.979294}},
+  {title: 'Metropolitan Museum of Art', location: {lat: 40.779437, lng: -73.963244}},
+  {title: 'Broadway and the Theater District', location: {lat: 40.758709, lng: -73.985367}},
+  {title: '9/11 Memorial and Museum', location: {lat: 40.711484, lng: -74.012725}},
+  {title: 'High Line', location: {lat: 40.747993,lng: -74.004765}},
+  {title: 'Times Square', location: {lat: 40.758895, lng: -73.985131}},
+  {title: 'Brooklyn Bridge', location: {lat: 40.706086, lng: -73.996864}},
+  {title: 'Friends Apartment', location: {lat: 40.732398, lng: -74.005317}},
+  {title: 'Fifth Avenue', location: {lat: 40.731412, lng: -73.996985}},
   {title: 'Empire state Building, NYC', location: {lat: 40.748817, lng: -73.985428}},
   {title: 'Bank of America Tower', location: {lat: 40.755604, lng: -73.984932}},
-  // {title: 'Federation Tower', location: {lat: 55.749695, lng: 37.537540}},
-  // {title: 'World Trade center', location: {lat: 24.487249, lng: 54.357464}},
-  // {title: 'Citic Plaza', location: {lat: 23.142359, lng: 113.324768}},
-  {title: 'Santa Monica Pier A', location: {lat: 34.010090, lng: -118.496948}},
-  {title: 'Trump International Hotel and Tower', location: {lat: 41.888866, lng: -87.626396}},
-  {title: 'Willis Tower', location: {lat: 41.878876, lng: -87.635918}},
   {title: 'One World Trade center', location: {lat: 40.712742, lng: -74.013382}},
-  {title: 'Naval Observatory Flag staff', location: {lat: 35.184017, lng: -111.740417}},
-  {title: 'Botanical Reasearch Institute of Texas', location: {lat: 32.742027, lng: -97.362564}},
   {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
   {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
   {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
   {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-  {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+  {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}},
+  {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}}
 ];
+
+
+var map;
+var infowindow;
+var bounds;
+
+initMap = function() {
+    map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 40.7413549, lng: -73.9980243999996},
+    zoom: 16,
+    mapTypeControl: false
+    });
+    infowindow = new google.maps.InfoWindow();
+    bounds = new google.maps.LatLngBounds();
+    createMarkers();
+
+}
+
+errorOnLoad = function() {
+  window.alert("There was an error in loading maps. Please try again..!!")
+}
 
 var ViewModel = function() {
 
   var self = this;
   this.search_val = ko.observable("");
   this.locations_list = ko.observableArray(locations_data);
-
-  var map;
   var markers = [];
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: 40.7413549, lng: -73.9980243999996},
-    zoom: 16,
-    mapTypeControl: false
+
+
+  this.updateList = function() {
+    hideMarkers();
+    value = self.search_val().toLowerCase();
+    self.locations_list([]);
+
+    locations_data.forEach(function(location) {
+      title = location.title.toLowerCase();
+      if (title.includes(value)){
+        self.locations_list.push(location);
+      }
     });
+    showMarkers();
+  }
 
-  var infowindow = new google.maps.InfoWindow();
 
-  var bounds = new google.maps.LatLngBounds();
+  this.showInfo = function(location) {
+    for(var i=0; i<markers.length; i++){
+      if(location.title == markers[i].title){
+        populateInfowindow(markers[i]);
+        break;
+      }
+    }
+  }
+
 
   this.toggleOptions = function(){
 
@@ -61,11 +90,13 @@ var ViewModel = function() {
     }
   }
 
+
   showMarkers = function() {
-    if (self.locations_list().length != 0){
+    var l = self.locations_list().length;
+    if (l != 0) {
       var j = 0;
-      for (var i=0; i<markers.length; i++){
-        if(self.locations_list()[j].title == markers[i].title){
+      for (var i=0; i<markers.length, j<l; i++) {
+        if(self.locations_list()[j].title == markers[i].title) {
           markers[i].setMap(map);
           bounds.extend(markers[i].position);
           j++;
@@ -75,11 +106,13 @@ var ViewModel = function() {
     }
   }
 
+
   hideMarkers = function() {
     for(var i=0; i<markers.length; i++){
       markers[i].setMap(null);
     }
   }
+
 
   makeMarkerIcon = function(color){
     var markerImage = new google.maps.MarkerImage(
@@ -91,14 +124,15 @@ var ViewModel = function() {
     return markerImage;
   }
 
+
   stopAnimation = function(marker) {
     setTimeout(function () {
         marker.setAnimation(null);
     }, 700);
   }
 
+
   populateInfowindow = function(marker) {
-    // console.log(marker);
     marker.setAnimation(google.maps.Animation.BOUNCE);
     stopAnimation(marker);
 
@@ -134,64 +168,38 @@ var ViewModel = function() {
 
       infowindow.open(map, marker);
     }
-  };
-
-  var defaultIcon = makeMarkerIcon('0091ff');
-  var highlightedIcon = makeMarkerIcon('FFFF24');
-
-  for(var i=0; i<self.locations_list().length; i++){
-    var position = self.locations_list()[i].location;
-    // console.log(position);
-    var title = self.locations_list()[i].title;
-    var marker = new google.maps.Marker({
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i,
-      icon: defaultIcon
-    });
-    markers.push(marker);
-    // console.log(marker.title);
-    // console.log(markers[i].position);
-    marker.addListener('click', function() {
-      populateInfowindow(this);
-    });
-
-    marker.addListener('mouseover', function(){
-      this.setIcon(highlightedIcon);
-    });
-
-    marker.addListener('mouseout', function(){
-      this.setIcon(defaultIcon);
-    });
   }
-  showMarkers();
 
-  this.updateList = function() {
-    hideMarkers();
-    value = self.search_val().toLowerCase();
-    self.locations_list([]);
 
-    locations_data.forEach(function(location) {
-      title = location.title.toLowerCase();
-      if (title.includes(value)){
-        self.locations_list.push(location);
-      }
-    });
+  createMarkers = function() {
+    var defaultIcon = makeMarkerIcon('0091ff');
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+
+    for(var i=0; i<self.locations_list().length; i++){
+      var position = self.locations_list()[i].location;
+      var title = self.locations_list()[i].title;
+      var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i,
+        icon: defaultIcon
+      });
+      markers.push(marker);
+      marker.addListener('click', function() {
+        populateInfowindow(this);
+      });
+
+      marker.addListener('mouseover', function(){
+        this.setIcon(highlightedIcon);
+      });
+
+      marker.addListener('mouseout', function(){
+        this.setIcon(defaultIcon);
+      });
+    }
     showMarkers();
   }
-
-  this.showInfo = function(location) {
-    for(var i=0; i<markers.length; i++){
-      console.log(markers[i].title);
-      if(location.title == markers[i].title){
-        populateInfowindow(markers[i]);
-        break;
-      }
-    }
-  }
-
-
 };
 
 
