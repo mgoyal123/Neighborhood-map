0f0742f0b53c131a0565926067c2652803c05bc9
Mishu Goyal <mishugoyal11@gmail.com>
2017-12-25 15:09:42 +0530

    Get initial data of location entered by user using foursquare api call

---
+++ README.md
@@ -0,0 +1,7 @@
+# Neighborhood maps
+### Quickstart
+To run the application, open _index.html_ in any browser.
+
+#### APIs used
+* Google maps javascript API - For Maps, Markers, Infowindows and Streetviews.
+* Foursquare API - For retrieving the details of individual locations.
--- js/app.js
+++ js/app.js
@@ -1,49 +1,62 @@
+// Client id and secret variables to make Foursquare api call
 var CLIENT_ID = "3SJU4035GEAO5AZRNFZUQWRN2XMTBM5BVZAHBYHOFLHHDPGX";
 var CLIENT_SECRET = "DF5HA2MSN4Z40LO2GJH0HV24Z2VEULR14MGI5HZKANEC1GLF"
 
-var locations_data = [
-  {title: 'Statue of Liberty', location: {lat: 40.689249, lng: -74.044500}},
-  {title: 'Central Park', location: {lat: 40.782865, lng: -73.965355}},
-  {title: 'Rockefeller Center and Top of the Rock Observation Deck', location: {lat: 40.759243, lng: -73.979294}},
-  {title: 'Metropolitan Museum of Art', location: {lat: 40.779437, lng: -73.963244}},
-  {title: 'Broadway and the Theater District', location: {lat: 40.758709, lng: -73.985367}},
-  {title: '9/11 Memorial and Museum', location: {lat: 40.711484, lng: -74.012725}},
-  {title: 'High Line', location: {lat: 40.747993,lng: -74.004765}},
-  {title: 'Times Square', location: {lat: 40.758895, lng: -73.985131}},
-  {title: 'Brooklyn Bridge', location: {lat: 40.706086, lng: -73.996864}},
-  {title: 'Friends Apartment', location: {lat: 40.732398, lng: -74.005317}},
-  {title: 'Fifth Avenue', location: {lat: 40.731412, lng: -73.996985}},
-  {title: 'Empire state Building, NYC', location: {lat: 40.748817, lng: -73.985428}},
-  {title: 'Bank of America Tower', location: {lat: 40.755604, lng: -73.984932}},
-  {title: 'One World Trade center', location: {lat: 40.712742, lng: -74.013382}},
-  {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-  {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-  {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-  {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-  {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}},
-  {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}}
-];
-
-
+// Global variable map
 var map;
+var locations_data = [];
 var infowindow;
-var bounds;
 
+// Callback function to initalize map on the screen
 initMap = function() {
     map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 40.7413549, lng: -73.9980243999996},
-    zoom: 16,
-    mapTypeControl: false
+    zoom: 13,
     });
     infowindow = new google.maps.InfoWindow();
-    bounds = new google.maps.LatLngBounds();
-    createMarkers();
-
 }
 
-
+// Callback error function if maps api does not load
 errorOnLoad = function() {
   window.alert("There was an error in loading maps. Please try again..!!")
+}
+
+// Method to get details of places near user entered location using foursquare api
+httpGet = function(entered_loc)
+{
+  theUrl = 'https://api.foursquare.com/v2/venues/explore?near=' + entered_loc + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20171224';
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+  xmlHttp.send( null );
+  var data = JSON.parse(xmlHttp.responseText);
+  locations_data = [];
+  if(data["meta"]["code"] == 200){
+    var base = data.response.groups[0].items;
+    var len = base.length;
+
+    for (var i=0; i<len; i++) {
+      var loc_details = {};
+      loc_details.title = base[i].venue.name;
+      loc_details.location = {
+        lat: base[i].venue.location.lat,
+        lng: base[i].venue.location.lng
+      };
+
+      if(base[i].venue.categories.name) {
+        loc_details.category = base[i].venue.categories.name;
+      }
+      if(base[i].venue.contact.phone) {
+        loc_details.contact = base[i].venue.contact.phone;
+      }
+      if(base[i].venue.url) {
+        loc_details.url = base[i].venue.url;
+      }
+      locations_data.push(loc_details);
+    }
+  }
+  else {
+    window.alert("Failed to retrive information for current location.Please try some other location..!!");
+  }
 }
 
 
@@ -51,10 +64,25 @@ var ViewModel = function() {
 
   var self = this;
   this.search_val = ko.observable("");
+  this.entered_loc = ko.observable("");
   this.locations_list = ko.observableArray(locations_data);
   var markers = [];
 
 
+  // This method is called when user enters a location and hits search button
+  this.showList = function() {
+    self.search_val("");
+    hideMarkers();
+    markers=[];
+    self.locations_list([]);
+    httpGet(self.entered_loc());
+    locations_data.forEach(function(location) {
+        self.locations_list.push(location);
+    });
+    createMarkers();
+  }
+
+  // This updates the ko observable array and markers according to the filter value
   this.updateList = function() {
     hideMarkers();
     value = self.search_val().toLowerCase();
@@ -69,7 +97,7 @@ var ViewModel = function() {
     showMarkers();
   }
 
-
+  // This is invoked when user hits a location from list
   this.showInfo = function(location) {
     for(var i=0; i<markers.length; i++){
       if(location.title == markers[i].title){
@@ -79,7 +107,7 @@ var ViewModel = function() {
     }
   }
 
-
+  // When hamburger icon is clicked, the options bar toggles
   this.toggleOptions = function(){
 
     if($('#options-box').is(":hidden")) {
@@ -95,8 +123,9 @@ var ViewModel = function() {
     }
   }
 
-
+  // This function will loop through the markers array and display them all.
   showMarkers = function() {
+    var bounds = new google.maps.LatLngBounds();
     var l = self.locations_list().length;
     if (l != 0) {
       var j = 0;
@@ -111,14 +140,16 @@ var ViewModel = function() {
     }
   }
 
-
+  // This function will loop through the listings and hide them all.
   hideMarkers = function() {
     for(var i=0; i<markers.length; i++){
       markers[i].setMap(null);
     }
   }
 
-
+  // This function takes in a COLOR, and then creates a new marker
+  // icon of that color. The icon will be 22 px wide by 36 high, have an origin
+  // of 0, 0 and be anchored at 10, 34).
   makeMarkerIcon = function(color){
     var markerImage = new google.maps.MarkerImage(
       'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+color+'|40|_|%E2%80%A2',
@@ -129,14 +160,14 @@ var ViewModel = function() {
     return markerImage;
   }
 
-
+  // This function stops the bounce animation of marker after 700 milliseconds
   stopAnimation = function(marker) {
     setTimeout(function () {
         marker.setAnimation(null);
     }, 700);
   }
 
-
+  // This function populates the infowindow with its street view and other details when the marker is clicked.
   populateInfowindow = function(marker) {
     marker.setAnimation(google.maps.Animation.BOUNCE);
     stopAnimation(marker);
@@ -149,74 +180,59 @@ var ViewModel = function() {
         infowindow.marker = null;
       });
 
-      var lat = marker.getPosition().lat();
-      var lng = marker.getPosition().lng();
       var content = "";
       var streetViewService = new google.maps.StreetViewService();
       var radius = 50;
+      var details = marker.customInfo;
+      if(details.category) {
+        content = '<div>Category:' + details.category + '</div><br>';
+      }
+      if(details.contact) {
+        content = content + '<div>Contact No.:' + details.contact + '</div><br>';
+      }
+      if(details.url) {
+        content = content + '<div><a target="_blank "href="' + details.url + '">For more information, Click here!</a></div>';
+      }
 
-      $.ajax({
-          type: "GET",
-          dataType: "jsonp",
-          timeout: 5000,
-          url: 'https://api.foursquare.com/v2/venues/explore?ll=' + lat + ',' + lng + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20171224',
-          success: function(data){
-            var base = data.response.groups[0].items[0];
-            if(base.venue.categories[0].name) {
-              var categories = base.venue.categories[0].name;
-              content = '<div id="category">Category:' + categories + '</div><br>';
+      function getStreetView(data, status) {
+        if( status == google.maps.StreetViewStatus.OK) {
+          var nearStreetViewLoc = data.location.latLng;
+          var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLoc, marker.position);
+          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>' + content);
+          var panoramaOptions = {
+            position: nearStreetViewLoc,
+            pov: {
+              heading: heading,
+              pitch: 30
             }
-            if(base.venue.contact.phone) {
-              var phone = base.venue.contact.phone;
-              content = content + '<div id="phone">Contact no:' + phone + '</div><br>';
-            }
-            if(base.venue.hours) {
-              if(base.venue.hours.status) {
-                var present_status = base.venue.hours.status;
-                content = content + '<div id="status">Status:' + present_status + '</div><br>';
-              }
-            }
-            if(base.venue.url) {
-              var url = base.venue.url;
-              content = content + '<div id="url"><a target="_blank "href="' + url + '">For more information, Click here!</a></div>';
-            }
+          };
 
-            function getStreetView(data, status) {
-              if( status == google.maps.StreetViewStatus.OK) {
-                var nearStreetViewLoc = data.location.latLng;
-                var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLoc, marker.position);
-                infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>' + content);
-                var panoramaOptions = {
-                  position: nearStreetViewLoc,
-                  pov: {
-                    heading: heading,
-                    pitch: 30
-                  }
-                };
-                var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
-              }
-              else{
-                infowindow.setContent("<div>" + marker.title + "</div><div> No street view found </div>" + content);
-              }
-            }
-            streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-            infowindow.open(map, marker);
-          },
-          error : function(data, staus) {
-            window.alert("Failed to retrive information for current Marker.Please try again later..!!");
-          }
-      });
+          var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+        }
+        else{
+          infowindow.setContent("<div>" + marker.title + "</div><div> No street view found </div>" + content);
+        }
+      }
+
+      // Use streetview service to get the closest streetview image within
+      // 50 meters of the markers position
+      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+      infowindow.open(map, marker);
     }
   }
 
-
+  // The following function uses the locations_list array to create an array of markers on initialize.
   createMarkers = function() {
     var defaultIcon = makeMarkerIcon('0091ff');
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
     var highlightedIcon = makeMarkerIcon('FFFF24');
 
     for(var i=0; i<self.locations_list().length; i++){
       var position = self.locations_list()[i].location;
       var title = self.locations_list()[i].title;
+
+      // Create a marker per location, and put into markers array.
       var marker = new google.maps.Marker({
         position: position,
         title: title,
@@ -224,7 +240,23 @@ var ViewModel = function() {
         id: i,
         icon: defaultIcon
       });
+      var customInfo = {};
+
+      if (self.locations_list()[i].url){
+        customInfo.url = self.locations_list()[i].url;
+      }
+      if (self.locations_list()[i].contact){
+        customInfo.contact = self.locations_list()[i].contact;
+      }
+      if (self.locations_list()[i].category){
+        customInfo.category = self.locations_list()[i].category;
+      }
+
+      marker.customInfo = customInfo;
+
       markers.push(marker);
+
+      // Create an onclick event to open the infowindow at each marker.
       marker.addListener('click', function() {
         populateInfowindow(this);
       });
@@ -239,7 +271,6 @@ var ViewModel = function() {
     }
     showMarkers();
   }
-
 };
 
 
